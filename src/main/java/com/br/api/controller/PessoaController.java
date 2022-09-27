3e94c4d1f44dd6074e55753b6725dd557d7018da
RafaelFernando12 <rafael.bergmann@hotmail.com>
2022-09-27 14:42:10 -0300

    :sparkles: Feature: CRUD

--- src/main/java/com/br/api/controller/PessoaController.java
+++ src/main/java/com/br/api/controller/PessoaController.java
@@ -1,17 +1,28 @@
 package com.br.api.controller;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.br.api.dto.PessoaDto;
+import com.br.api.repository.PessoaRepository;
 import com.br.api.service.PessoaService;
+
+import lombok.Delegate;
 
 @RestController
 @RequestMapping("/pessoa")
@@ -34,5 +45,24 @@ public class PessoaController {
 				.buildAndExpand(pessoa.getId()).toUri();
 		return ResponseEntity.created(uri).body(pessoa);
 	}
+		
+	@GetMapping()
+	ResponseEntity<List<PessoaDto>> listar(){
+		return ResponseEntity.ok(pessoaService.listar());
+	}
 	
+	@GetMapping("/listar/paginado")
+	ResponseEntity<Page<PessoaDto>> listarPaginado(Pageable page){
+		return ResponseEntity.ok(pessoaService.listarPaginado(page));
+	}
+	
+	@PutMapping()
+	ResponseEntity<PessoaDto> atualiza(@RequestBody PessoaDto pessoaDto){
+		return ResponseEntity.ok(pessoaService.atualizar(pessoaDto));
+	}
+	
+	@DeleteMapping("/{id}")
+	ResponseEntity<Long> deletar(@PathVariable Long id){
+		return ResponseEntity.ok(pessoaService.deletar(id));
+	}
 }
--- src/main/java/com/br/api/service/PessoaService.java
+++ src/main/java/com/br/api/service/PessoaService.java
@@ -1,22 +1,69 @@
 package com.br.api.service;
 
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.br.api.dto.PessoaDto;
 import com.br.api.model.Pessoa;
 import com.br.api.repository.PessoaRepository;
+import com.br.api.util.BeanUtil;
+
 
 @Service
 public class PessoaService {
 	
 	@Autowired
 	PessoaRepository pessoaRepository;
-
+	
+	public Pessoa save(Pessoa pessoa) {
+		return pessoaRepository.save(pessoa);
+	}
+	
 	public PessoaDto salvar(PessoaDto pessoaDto) {
-		Pessoa pessoa = new Pessoa(pessoaDto);
-		pessoa = pessoaRepository.save(pessoa);
+	Pessoa pessoa = this.save(new Pessoa(pessoaDto));
 		return new PessoaDto(pessoa);
 	}
+	
+	public Pessoa findById(Long id) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(id);
+		if(pessoa.isPresent()) {
+			return pessoa.get();
+		}
+		return null;
+	}
 
+	public PessoaDto atualizar(PessoaDto pessoaDto) {
+		Pessoa objBanco = this.findById(pessoaDto.getId());
+		BeanUtils.copyProperties(pessoaDto, objBanco, BeanUtil.getNull(pessoaDto));
+		objBanco = this.save(objBanco); 
+		return new PessoaDto(objBanco);
+	}
+
+	public List<PessoaDto> listar() {
+		List<Pessoa> pessoas = pessoaRepository.findAll();
+		return pessoas.stream().map(PessoaDto :: new).toList();
+	}
+
+	public Page<PessoaDto> listarPaginado(Pageable page) {
+		Page<Pessoa> pagePessoa = pessoaRepository.findAll(page);
+		Page<PessoaDto> pagePessoaDto = pagePessoa.map(p -> {
+			PessoaDto pessoa = new PessoaDto(p);
+			return pessoa;
+		});
+		return pagePessoaDto;
+	}
+
+	public Long deletar(Long id) {
+		Pessoa pessoa = this.findById(id);
+		pessoaRepository.delete(pessoa);
+		return id;
+	}
 }
